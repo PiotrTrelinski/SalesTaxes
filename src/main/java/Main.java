da537bfc60c332fff824166375da6b8b9ca738c1
Sebastian Zielinski <sebek59@gmail.com>
2017-04-05 15:15:49 +0200

    XML reading finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
+import org.w3c.dom.NodeList;
+
+import helpers.XmlReader;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
 /**
  * Created by Maciek on 05.04.2017.
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Aplikacja v.1.0");
+        XmlReader xml = new XmlReader("C:\\Users\\student\\ZPI\\SalesTaxes\\src\\main\\resources\\data\\data.xml");
+        xml.read();
+        NodeList states = xml.getStatesList();
+        
+        for (int i = 0; i<states.getLength();i++) {
+        	System.out.println(states.item(i).getTextContent());
+        }
     }
 }
--- src/main/java/helpers/XmlReader.java
+++ src/main/java/helpers/XmlReader.java
@@ -1,17 +1,18 @@
 package helpers;
-import java.io.File;
-
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-
+import javax.xml.parsers.DocumentBuilder;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
 
 public class XmlReader {
 	
 	public XmlReader(String path) {
 		this.fileName=path;
 	}
+	private Document doc;
 	
 	private String fileName;
 	private NodeList statesList;
@@ -21,17 +22,24 @@ public class XmlReader {
 			File fXmlFile = new File(fileName);
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(fXmlFile);
+			doc = dBuilder.parse(fXmlFile);
 			doc.getDocumentElement().normalize();
 			statesList = doc.getElementsByTagName("state");
 		}
 		catch (Exception e) {
+			e.printStackTrace();
 			throw e;
 		}
 	}
 
 	public NodeList getStatesList() {
 		return statesList;
+	}
+	
+	public int getValue(String state, String category, String product) {
+		
+		
+		return 0;
 	}
 
 }
