4316056cf9c602446b22de42eca696a8755313e6
Maciek <miedzianowski.maciek@gmail.com>
2017-05-10 14:19:03 +0200

    Wstepne wczytywanie xml'a i wypisywanie na konsole

--- src/main/java/HelloJavaFxAndMavenApp.java
+++ src/main/java/HelloJavaFxAndMavenApp.java
@@ -1,20 +1,32 @@
+import helpers.XmlReader;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.w3c.dom.NodeList;
 
 public class HelloJavaFxAndMavenApp extends Application {
 
 
     public static void main(String[] args) throws Exception {
+
+        System.out.println("Aplikacja v.1.0");
+        XmlReader xml = new XmlReader("C:\\ZPI\\src\\main\\resources\\data\\data.xml");
+        xml.read();
+        NodeList states = xml.getStatesList();
+
+        for (int i = 0; i<states.getLength();i++) {
+            System.out.println(states.item(i).getTextContent());
+        }
+
         launch(args);
     }
 
     public void start(Stage stage) throws Exception {
 
 
-        String fxmlFile = "/fxml/hello.fxml";
+        String fxmlFile = "/fxml/hello2.fxml";
         FXMLLoader loader = new FXMLLoader();
         Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
 
