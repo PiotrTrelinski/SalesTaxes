6642589425a36c07d1febb6a437c19f2fe92acfe
Maciek <miedzianowski.maciek@gmail.com>
2017-05-10 15:15:43 +0200

    Przygotowanie obliczania ceny

--- src/main/java/HelloController.java
+++ src/main/java/HelloController.java
@@ -1,8 +1,12 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.*;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 
+import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -12,6 +16,7 @@ public class HelloController
     @FXML ChoiceBox categoryList;
     @FXML ChoiceBox productList;
     @FXML ChoiceBox stateList;
+    @FXML Button button;
 
 
     @FXML
@@ -21,6 +26,7 @@ public class HelloController
         listWithCategories.add("test1");
         initializeCategoryList(listWithCategories);
         System.out.println("second");
+        button.setOnMouseClicked(event -> buttonClickedFunciton());
     }
 
     private void initializeCategoryList(Collection collection) {
@@ -48,5 +54,9 @@ public class HelloController
         collection.forEach(item -> observableList.add(item));
 
         stateList.setItems(observableList);
+    }
+
+    private void buttonClickedFunciton() {
+        categoryList.getSelectionModel().getSelectedItem().toString();
     }
 }
