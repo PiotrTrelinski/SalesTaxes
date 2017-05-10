747d0486d0aae10ea20db4495cd8268b1ccfadf6
Sebastian Zielinski <sebek59@gmail.com>
2017-05-10 15:22:27 +0200

    Added parsing produc xml to list of objects

--- src/main/java/objects/Product.java
+++ src/main/java/objects/Product.java
@@ -9,4 +9,23 @@ public class Product {
 		this.category = category;
 		this.price = price;
 	}
+	public Product() {}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
 }
